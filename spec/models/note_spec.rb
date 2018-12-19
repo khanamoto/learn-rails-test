c1f7fce425488e9c35b3f1bfbdbef712e58772bb
Kenji Hanamoto <khanamoto10@gmail.com>
2018-12-20 00:15:27 +0900

    refactor: Make tests DRY

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,60 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  it "検索文字列に一致するメモを返すこと" do
-    user = User.create(
-      first_name: "Jow",
+  # このファイルの全テストで使用するデータをセットする
+  before do
+    @user = User.create(
+      first_name: "Jows",
       last_name: "Yamamoto",
       email: "jow@example.com",
       password: "111111",
     )
 
-    project = user.projects.create(
+    @project = @user.projects.create(
       name: "Test Project",
     )
-
-    note1 = project.notes.create(
-      message: "This is the first note.",
-      user: user,
-    )
-    note2 = project.notes.create(
-      message: "This is the second note.",
-      user: user,
-    )
-    note3 = project.notes.create(
-      message: "First, preheat the oven.",
-      user: user,
-    )
-
-    expect(Note.search("first")).to include(note1, note3)
-    expect(Note.search("first")).to_not include(note2)
   end
 
-  it "検索結果が1件も見つからなければ空のコレクションを返すこと" do
-    user = User.create(
-      first_name: "Mike",
-      last_name: "Banana",
-      email: "test@example.com",
-      password: "000000",
+  it "ユーザー、プロジェクト、メッセージがあれば有効な状態であること" do
+    note = Note.new(
+      message: "This is a sample note.",
+      user: @user,
+      project: @project,
     )
+    expect(note).to be_valid
+  end
 
-    project = user.projects.create(
-      name: "Test Project",
-    )
+  it "メッセージがなければ無効な状態であること" do
+    note = Note.new(message: nil)
+    note.valid?
+    expect(note.errors[:message]).to include("can't be blank")
+  end
 
-    note1 = project.notes.create(
-      message: "This is the first note.",
-      user: user,
-    )
-    note2 = project.notes.create(
-      message: "This is the second note.",
-      user: user,
-    )
-    note3 = project.notes.create(
-      message: "First, preheat oven.",
-      user: user,
-    )
+  describe "文字列に一致するメッセージを検索する" do
+    # 検索機能の全テストに関する追加のテストデータをセットする
+    before do
+      @note1 = @project.notes.create(
+        message: "This is the first note.",
+        user: @user,
+      )
+      @note2 = @project.notes.create(
+        message: "This is the second note.",
+        user: @user,
+      )
+      @note3 = @project.notes.create(
+        message: "First, preheat oven.",
+        user: @user,
+      )
+    end
 
-    expect(Note.search("message")).to be_empty
+    context "一致するデータが見つかるとき" do
+      it "検索文字列に一致するメモを返すこと" do
+        expect(Note.search("first")).to include(@note1, @note3)
+      end
+    end
+
+    context "一致するデータが1件も見つからないとき" do
+      it "空のコレクションを返すこと" do
+        expect(Note.search("message")).to be_empty
+      end
+    end
   end
 end
