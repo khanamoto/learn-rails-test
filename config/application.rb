3b82ec59357948325eb7893ba0f6c78ea3444b63
Kenji Hanamoto <khanamoto10@gmail.com>
2018-12-22 21:14:24 +0900

    chore: Add gem

--- config/application.rb
+++ config/application.rb
@@ -32,10 +32,10 @@ module RailsTestLearn
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false
+        routing_specs: false,
+        request_specs: false
     end
   end
 end
