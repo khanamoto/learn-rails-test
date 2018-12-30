bd4ab5cda9884fe941a0dfbbf56d620645766d5c
Kenji Hanamoto <khanamoto10@gmail.com>
2018-12-30 16:45:36 +0900

    feat: Add chap 7

---
+++ spec/requests/projects_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Projects", type: :request do
+  context "認証済みのユーザーとして" do
+    before do
+      @user = FactoryBot.create(:user)
+    end
+
+    context "有効な属性値の場合" do
+      # TODO: 本来は1つ追加できるはずだが追加できずに失敗する
+      it "プロジェクトを追加できること" do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        expect {
+          post projects_path, params: { project: project_params }
+        }.to change(@user.projects, :count).by(1)
+      end
+    end
+
+    context "無効な属性値の場合" do
+      it "プロジェクトを追加できないこと" do
+        project_params = FactoryBot.attributes_for(:project, :invalid)
+        sign_in @user
+        expect {
+          post projects_path, params: { project: project_params }
+        }.to_not change(@user.projects, :count)
+      end
+    end
+  end
+end
---
+++ spec/support/request_spec_helper.rb
@@ -0,0 +1,22 @@
+module RequestSpecHelper
+  include Warden::Test::Helpers
+
+  def self.included(base)
+    base.before(:each) { Warden.test_mode! }
+    base.after(:each) { Warden.test_reset! }
+  end
+
+  def sign_in(resourse)
+    login_as(resourse, scope: warden_scope(response))
+  end
+
+  def sign_out(resourse)
+    logout(warden_scope(resourse))
+  end
+
+  private
+
+  def warden_scope(resourse)
+    resourse.class.name.underscore.to_sym
+  end
+end
