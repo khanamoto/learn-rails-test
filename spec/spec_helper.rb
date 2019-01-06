d3416c209444bd866640274cf32767afeb882fd9
Kenji Hanamoto <khanamoto10@gmail.com>
2019-01-06 21:54:25 +0900

    chore: Change filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -95,8 +95,8 @@ RSpec.configure do |config|
 =end
 
   # focusタグがあればそれを利用する、1つもなければ全体を実行する
-  config.filter_run focus: true
-  config.run_all_when_everything_filtered = true
+  # config.filter_run focus: true
+  # config.run_all_when_everything_filtered = true
 
   # slowタグがついたexampleは常にスキップする
   # config.filter_run_excluding slow: true
