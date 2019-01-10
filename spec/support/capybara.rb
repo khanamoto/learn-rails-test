f5522470b2da62c85a3393409fa44481d1cc3736
Kenji Hanamoto <khanamoto10@gmail.com>
2019-01-10 23:58:17 +0900

    feat: Change

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,3 +1,13 @@
 # Capybara.javascript_driver = :selenium_chrome
-Capybara.javascript_driver = :selenium_chrome_headless
+# Capybara.javascript_driver = :selenium_chrome_headless
 # Capybara.default_max_wait_time = 15
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
+end
